04cc2c511f9ff63ce24dfc2f9d3795d02761bb37
Erick Francis <erickfrancis.zip@gmail.com>
2022-10-26 19:41:32 -0300

    Estrutura inicial do App
    Cria o app utilizando o flutter create e deixa somente algumas informações para funcionar no Android

---
+++ app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'World Cities',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('World Cities'),
+      ),
+    );
+  }
+}
