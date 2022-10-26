00f1a797997e93f5eadf67f79103a183bf1060b3
Erick Francis <erickfrancis.zip@gmail.com>
2022-10-26 20:31:17 -0300

    Adiciona dependência firestore

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
@@ -27,11 +36,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late FirebaseFirestore firestore;
+  bool loading = false;
+
+  fetch() {
+// // Create a new user with a first and last name
+//     final user = <String, dynamic>{
+//       "first": "Ada",
+//       "last": "Lovelace",
+//       "born": 1815
+//     };
+
+// // Add a new document with a generated ID
+//     firestore.collection("world_cities").add(user).then(
+//           (DocumentReference doc) =>
+//               print('DocumentSnapshot added with ID: ${doc.id}'),
+//         );
+
+    firestore.collection('world_cities').get().then((event) {
+      for (var doc in event.docs) {
+        print("${doc.id} => ${doc.data()}");
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    firestore = FirebaseFirestore.instance;
+
+    super.initState();
+
+    fetch();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('World Cities'),
+        title: const Text('World Cities'),
       ),
     );
   }
